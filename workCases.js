bdea5a87b015fd963de859f8633f3193352ccb2f
jaanreymond <johnaempuerto@gmail.com>
2020-12-07 15:14:56 +0800

    initial commit

--- workCases.js
+++ workCases.js
@@ -1,8 +1,41 @@
 export const workCases = [
   {
+    title: "Nordsjælland Osteopati & Fysioterapi",
+    category: "WordPress Website",
+    description: "Nordsjælland Osteopati & Fysioterapi consists of Jakob Aagaard, who does osteopathic and physiotherapeutic treatments at his clinic in Farum, Denmark. His needs were a WordPress website for information and booking, which was built with Elementor.",
+    caseBackground: "/images/tablet-background.webp",
+    caseImage: "/images/nordsjaelland-osteopati-case-tablet.webp",
+    caseImageMobile: "/images/nordsjaelland-osteopati-project-mobile.webp",
+    caseImageAlt: "nordsjaelland osteopati og fysioterapi wordpress website by John Empuerto",
+    caseLink: "https://nordsjaelland-osteopati.dk/",
+    caseLinkText: "Visit site",
+  },
+  {
+    title: "My Virtual Workforce",
+    category: "WordPress Website",
+    description: "My Virtual Workforce is a platform where virtual employees are hired for suitable jobs, the site was built with Elementor.",
+    caseBackground: "/images/tablet-background.webp",
+    caseImage: "/images/myvirtualwork-case-tablet.webp",
+    caseImageMobile: "/images/myvirtualwork-project-mobile.webp",
+    caseImageAlt: "myvirtualworkforce wordpress website by John Empuerto",
+    caseLink: "http://myvirtualworkforce.com.au/",
+    caseLinkText: "Visit site",
+  },
+  {
+    title: "Personal Website V1",
+    category: "Web Development",
+    description: "My first personal website built with HTML, CSS, Javascript and hosted on GitHub Pages.",
+    caseBackground: "/images/tablet-background.webp",
+    caseImage: "/images/johnempuerto-case-tablet.webp",
+    caseImageMobile: "/images/johnempuerto-project-mobile.webp",
+    caseImageAlt: "Personal Website by John Empuerto",
+    caseLink: "https://jaanreymond.github.io/johnempuertoo/",
+    caseLinkText: "Visit site",
+  },
+  {
     title: "ExploraOne",
     category: "Web Development",
-    description: `ExploraOne is a Filipino travel agency that offers affordable quality trips all over the world. The work I do for this site consists of helping them building/maintaining their Site with multiple language layers.`,
+    description: `ExploraOne is a Filipino travel agency that offers affordable quality trips all over the world. The work I do for this site consists of helping them building/maintaining their Site with multiple language layers. `,
     caseBackground: "/images/tablet-background.webp",
     caseImage: "/images/explora-case-tablet.webp",
     caseImageMobile: "/images/explora-project-mobile.webp",
@@ -23,19 +56,9 @@ export const workCases = [
     caseLink: "#",
     caseLinkText: "Visit site",
   },
+  
   {
-    title: "Nordsjælland Osteopati & Fysioterapi",
-    category: "WordPress Website",
-    description: "Nordsjælland Osteopati & Fysioterapi consists of Jakob Aagaard, who does osteopathic and physiotherapeutic treatments at his clinic in Farum, Denmark. His needs were a WordPress website for information and booking, which was built with Elementor.",
-    caseBackground: "/images/tablet-background.webp",
-    caseImage: "/images/nordsjaelland-osteopati-case-tablet.webp",
-    caseImageMobile: "/images/nordsjaelland-osteopati-project-mobile.webp",
-    caseImageAlt: "nordsjaelland osteopati og fysioterapi wordpress website by John Empuerto",
-    caseLink: "https://nordsjaelland-osteopati.dk/",
-    caseLinkText: "Visit site",
-  },
-  {
-    title: "Github Search Users",
+    title: "GitHub Search Users",
     category: "Web Application",
     description: "A nicer look at your GitHub profile and repository stats with data visualizations of your top languages and stars. Sort through your top repos by number of stars, forks, and size.",
     caseBackground: "/images/tablet-background.webp",
@@ -45,17 +68,7 @@ export const workCases = [
     caseLink: "https://githubusersearchreact.netlify.app/",
     caseLinkText: "Visit site",
   },
-  {
-    title: "Personal Website V1",
-    category: "Web Development",
-    description: "My first personal website built with HTML, CSS, Javascript and hosted on GitHub Pages.",
-    caseBackground: "/images/tablet-background.webp",
-    caseImage: "/images/johnempuerto-case-tablet.webp",
-    caseImageMobile: "/images/johnempuerto-project-mobile.webp",
-    caseImageAlt: "Personal Website by John Empuerto",
-    caseLink: "https://jaanreymond.github.io/johnempuertoo/",
-    caseLinkText: "Visit site",
-  },
+  
 ]
 
 export default workCases;
